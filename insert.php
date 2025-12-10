5b8c0f5d13beb9490b95e5512ba473ca552b68a4
JackONeill03 <B00155573@mytudublin.ie>
2025-12-10 00:16:11 +0000

    Updated HTML Form

---
+++ insert.php
@@ -0,0 +1,23 @@
+<?php
+
+include("dbcon.php");
+
+$vd = $_POST['visitDate']; 
+$vt = $_POST['visitTime']; 
+$di = (int)$_POST['doctorid']; 
+$st = (int)$_POST['patientid']; 
+$et = $_POST['cost'];
+
+$sql = "INSERT INTO visit (user_id,,doctor_id,patient_id,cost) values ('$vd','$vt','$di','$st','$et')";
+
+//echo $sql;
+
+if (mysqli_query($conn, $sql)) {
+  echo "<br>New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+
+mysqli_close($conn); 
+
+?>
